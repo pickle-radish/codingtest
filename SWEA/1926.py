0641d7fc177699bfa0c5f7e578a278e5322b9921
pickle-radish <tkdansg0222@gmail.com>
2020-06-27 10:40:43 +0900

    3 6 9 game

--- SWEA/1926.py
+++ SWEA/1926.py
@@ -1,1 +1,18 @@
-1926.py
+import sys
+sys.stdin = open('input_' + sys.argv[0][:-3] + '.txt')
+
+number = int(input())
+
+for n in range(1, number+1):
+    clapNumber = [3,6,9]
+    clap = False
+    clapCount = 0
+    for i in str(n): 
+        if int(i) in clapNumber:
+            clapCount+=1
+            clap=True
+
+    if clap:
+        print("-" * clapCount, end=" ")
+    else:
+        print(n, end=" ")
